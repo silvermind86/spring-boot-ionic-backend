b76b68cb685c1f9d9343448dd25db6797b00b53f
Jonas Magno <jmagno.costa@gmail.com>
2018-10-27 11:23:37 -0300

    Adicionando o endpoint de pedidos

--- src/main/java/com/jonasmagno/cursomc/services/PedidoService.java
+++ src/main/java/com/jonasmagno/cursomc/services/PedidoService.java
@@ -1,12 +1,15 @@
 package com.jonasmagno.cursomc.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.jonasmagno.cursomc.domain.Categoria;
 import com.jonasmagno.cursomc.domain.Pedido;
 import com.jonasmagno.cursomc.repositories.PedidoRepository;
+import com.jonasmagno.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class PedidoService {
@@ -16,6 +19,12 @@ public class PedidoService {
 	
 	public List<Pedido> salvar(List<Pedido> pedidos) {
 		return repo.saveAll(pedidos);
+	}
+	
+	public Pedido buscar(Integer id) {
+	 	Optional<Pedido> ped = repo.findById(id);
+	 	return ped.orElseThrow(() -> new ObjectNotFoundException(
+			"Obejto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
 
 }
