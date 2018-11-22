788be923323a0bf7aacf5fa85c25bdc9e565bfc4
Jonas Magno <jmagno.costa@gmail.com>
2018-11-22 21:56:54 -0200

    Adicionando paginação do spring data, com parametros adicionais.

--- src/main/java/com/jonasmagno/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/jonasmagno/cursomc/resources/CategoriaResource.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -64,6 +66,21 @@ public class CategoriaResource {
 		List<Categoria> lista = service.buscarTodos();
 		List<CategoriaDTO> categorias = lista.stream().map(
 			Obj -> new CategoriaDTO(Obj)).collect(Collectors.toList());
+		if (categorias == null) {
+			return ResponseEntity.unprocessableEntity().body(null);
+		}
+		return ResponseEntity.ok().body(categorias);
+	}
+	
+	@RequestMapping(value="/page",method=RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+		@RequestParam(value="page",defaultValue="0") Integer pagina, 
+		@RequestParam(value="linesPerPage",defaultValue="24") Integer linhasPorPagina, 
+		@RequestParam(value="orderBy",defaultValue="nome") String ordenar, 
+		@RequestParam(value="direction",defaultValue="ASC") String sentido) {
+		
+		Page<Categoria> lista = service.buscaPagina(pagina, linhasPorPagina, ordenar, sentido);
+		Page<CategoriaDTO> categorias = lista.map(Obj -> new CategoriaDTO(Obj));
 		if (categorias == null) {
 			return ResponseEntity.unprocessableEntity().body(null);
 		}
--- src/main/java/com/jonasmagno/cursomc/services/CategoriaService.java
+++ src/main/java/com/jonasmagno/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.jonasmagno.cursomc.domain.Categoria;
@@ -46,5 +49,13 @@ public class CategoriaService {
 
 	public List<Categoria> buscarTodos() {
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> buscaPagina(
+		Integer pagina, Integer linhasPorPagina, String ordenar, String sentido) {
+		
+		PageRequest page = PageRequest.of(
+			pagina, linhasPorPagina, Direction.valueOf(sentido), ordenar);
+		return repo.findAll(page);
 	}
 }
